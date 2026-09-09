e94ef62c448a625641ed15148479c01a34898d1b
Ntando Miya <ntandomiya97@gmail.com>
2026-09-09 15:04:39 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/enviro/assessment/junior/ntandomiya/NtandomiyaApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.ntandomiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtandomiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtandomiyaApplication.class, args);
+	}
+
+}
