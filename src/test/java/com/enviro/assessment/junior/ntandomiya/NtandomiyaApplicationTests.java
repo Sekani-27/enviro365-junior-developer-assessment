5fd91822b390cee20849df9c31c9ecd79093ac95
Ntando Miya <ntandomiya97@gmail.com>
2026-09-10 11:02:18 +0200

    test: add Spring application context test

---
+++ src/test/java/com/enviro/assessment/junior/ntandomiya/NtandomiyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.ntandomiya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtandomiyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
